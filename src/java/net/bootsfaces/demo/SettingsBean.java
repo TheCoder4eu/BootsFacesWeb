f6fa43eb444637c2922aa749c3326a7e54b12241
stephanrauh <stephan.rauh@gmx.net>
2014-12-01 23:40:40 +0100

    started to document b:panelGrid

---
+++ src/java/net/bootsfaces/demo/SettingsBean.java
@@ -0,0 +1,66 @@
+/**
+ *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.bootsfaces.demo;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+/** A simple bean for demo purposes. */
+@RequestScoped
+@ManagedBean
+public class SettingsBean {
+	private int value1;
+
+	private int value2;
+
+	private boolean checkbox1;
+	private boolean checkbox2;
+
+	public int getValue1() {
+		return value1;
+	}
+
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+
+	public int getValue2() {
+		return value2;
+	}
+
+	public void setValue2(int value2) {
+		this.value2 = value2;
+	}
+
+	public boolean isCheckbox1() {
+		return checkbox1;
+	}
+
+	public void setCheckbox1(boolean checkbox1) {
+		this.checkbox1 = checkbox1;
+	}
+
+	public boolean isCheckbox2() {
+		return checkbox2;
+	}
+
+	public void setCheckbox2(boolean checkbox2) {
+		this.checkbox2 = checkbox2;
+	}
+
+}
