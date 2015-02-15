1e048aa94554c4acb278ecf5abe4f89cc7407cc7
stephanrauh <stephan.rauh@gmx.net>
2015-02-15 23:37:20 +0100

    Completed the PrimeFaces barcode demo

---
+++ src/java/net/bootsfaces/demo/BarcodeBean.java
@@ -0,0 +1,52 @@
+/**
+ *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bootsfaces.demo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.validation.constraints.Pattern;
+
+@ViewScoped
+@ManagedBean
+public class BarcodeBean {
+	private String code39="1234567890128";
+	private String protocol="http://";
+	private String url = "angularfaces.net";
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getProtocol() {
+		return protocol;
+	}
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+	public String getCode39() {
+		return code39;
+	}
+	public void setCode39(String code39) {
+		this.code39 = code39;
+	}
+	public String getCompleteURL() {
+		return protocol + "www." + url;
+	}
+	
+
+}
