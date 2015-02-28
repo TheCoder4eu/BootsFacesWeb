eaff3aff5c3d0b957757d2e663e80fac5285ee86
droidcoder <droidcoder@thecoder4eu>
2015-02-28 19:23:38 +0100

    Adding the DashBoard example ( #62 )

---
+++ src/java/net/bootsfaces/demo/LoremIpsumBean.java
@@ -0,0 +1,67 @@
+package net.bootsfaces.demo;
+
+/**
+ * Managed Bean for Items
+ * @author Thecoder4.eu
+ */
+
+import java.util.StringTokenizer;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.model.ArrayDataModel;
+import javax.faces.model.DataModel;
+
+/**
+ * Data Model Bean for Items
+ * @author Thecoder4.eu
+ */
+@ManagedBean
+@RequestScoped
+public class LoremIpsumBean {
+    
+    public static final String loremipsum="Lorem ipsum dolor sit amet "
+            + "consectetur adipiscing elit sed "
+            + "do eiusmod tempor incididunt "
+            + "ut labore et dolore magna aliqua "
+            + "Ut enim ad minim veniam "
+            + "quis nostrud exercitation ullamco laboris "
+            + "nisi ut aliquip ex ea commodo consequat "
+            + "Duis aute irure dolor in reprehenderit in voluptate "
+            + "velit esse cillum dolore eu fugiat nulla pariatur "
+            + "Excepteur sint occaecat cupidatat non proident "
+            + "sunt in culpa qui officia deserunt mollit anim id est laborum";
+    
+    private static LoremItem[] itemList;
+    private DataModel<LoremItem> items;
+
+    public LoremIpsumBean() {
+        StringTokenizer st = new StringTokenizer(loremipsum);
+        itemList = new LoremItem[]{
+            new LoremItem("1,001", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,002", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,003", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,004", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,005", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,006", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,007", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,008", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,009", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,010", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,011", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,012", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,013", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,014", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken()),
+            new LoremItem("1,015", st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken())
+        };
+        items = new ArrayDataModel<LoremItem>(itemList);
+    }
+    
+    public DataModel<LoremItem> getItems() {
+        return items;
+    }
+
+    public void setItems(DataModel<LoremItem> items) {
+        this.items = items;
+    }
+    
+}
---
+++ src/java/net/bootsfaces/demo/LoremItem.java
@@ -0,0 +1,62 @@
+package net.bootsfaces.demo;
+
+/**
+ * Store Items Class
+ * @author Thecoder4.eu
+ */
+public class LoremItem {
+    private String h1;
+    private String h2;
+    private String h3;
+    private String h4;
+    private String h5;
+
+    public LoremItem(String h1, String h2, String h3, String h4, String h5) {
+        this.h1 = h1;
+        this.h2 = h2;
+        this.h3 = h3;
+        this.h4 = h4;
+        this.h5 = h5;
+    }
+
+    public String getH1() {
+        return h1;
+    }
+
+    public void setH1(String h1) {
+        this.h1 = h1;
+    }
+
+    public String getH2() {
+        return h2;
+    }
+
+    public void setH2(String h2) {
+        this.h2 = h2;
+    }
+
+    public String getH3() {
+        return h3;
+    }
+
+    public void setH3(String h3) {
+        this.h3 = h3;
+    }
+
+    public String getH4() {
+        return h4;
+    }
+
+    public void setH4(String h4) {
+        this.h4 = h4;
+    }
+
+    public String getH5() {
+        return h5;
+    }
+
+    public void setH5(String h5) {
+        this.h5 = h5;
+    }
+
+}
