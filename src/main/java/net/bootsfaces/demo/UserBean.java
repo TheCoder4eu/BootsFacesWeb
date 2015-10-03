9b7084f791e419779f72d0cd0cdeeb87930fc7a9
stephan <webmaster1@beyondjava.de>
2015-10-03 16:11:28 +0200

    added the CSS classes bf-required, bf-info, bf-warning, bf-error, bf-fatal and bf-no-message to input fields and their labels

---
+++ src/main/java/net/bootsfaces/demo/UserBean.java
@@ -0,0 +1,35 @@
+package net.bootsfaces.demo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@ManagedBean
+@RequestScoped
+public class UserBean {
+	@Size(min=1, max=10)
+	private String name;
+	
+//	@Min(value=18, message="In most countries, you're not considered a grown-up yet.")
+//	@Max(value=65, message="In many countries, you've already reached retirment age.")
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
