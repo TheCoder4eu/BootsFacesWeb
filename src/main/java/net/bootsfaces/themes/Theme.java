b14b320a71fc0b3e1541527b8ac3ecfbcbcad3a5
stephan <webmaster1@beyondjava.de>
2015-12-09 08:52:00 +0100

    added a theme switcher and two custom themes

---
+++ src/main/java/net/bootsfaces/themes/Theme.java
@@ -0,0 +1,47 @@
+package net.bootsfaces.themes;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Theme implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String currentTheme="default";
+	
+	private String internalTheme="default";
+	
+	private String customTheme="";
+
+	public String getCurrentTheme() {
+		return currentTheme;
+	}
+	public String getInternalTheme() {
+		return internalTheme;
+	}
+	public String getCustomTheme() {
+		return customTheme;
+	}
+
+	public void setCurrentTheme(String currentTheme) {
+		this.currentTheme = currentTheme;
+		if (currentTheme.equalsIgnoreCase("Freelancer")) {
+			internalTheme="other";
+			customTheme=currentTheme;
+		} else if (currentTheme.equalsIgnoreCase("Grayscale")) {
+			internalTheme="other";
+			customTheme=currentTheme;
+		} else {
+			internalTheme = currentTheme;
+			customTheme="";
+		}
+	}
+	
+	public void selectTheme() {
+		System.out.println("Theme selected");
+	}
+
+}
