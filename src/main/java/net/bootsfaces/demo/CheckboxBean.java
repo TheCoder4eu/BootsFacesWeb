264d09ae911d54f0b3b4e5fbd1897452ee0f5939
stephan <webmaster1@beyondjava.de>
2015-12-27 20:06:11 +0100

    Update to version 0.8.1 added an example for b:message

--- src/main/java/net/bootsfaces/demo/CheckboxBean.java
+++ src/main/java/net/bootsfaces/demo/CheckboxBean.java
@@ -2,20 +2,33 @@ package net.bootsfaces.demo;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.validation.constraints.AssertTrue;
 
 @SessionScoped
 @ManagedBean
 public class CheckboxBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private boolean checkbox = true;
-	
+
+	@AssertTrue(message = "Greetings from the JSR303 Bean Validation API: This checkbox must always be checked!")
+	private boolean cbxError = false;
+	private boolean cbxFatal = false;
+	private boolean cbxWarning = false;
+	private boolean cbxInfo = false;
+
 	private boolean editable = false;
-	
+
+	public CheckboxBean() {
+		setCbxError(true);
+	}
+
 	public boolean isEditable() {
 		return editable;
 	}
@@ -25,7 +38,7 @@ public class CheckboxBean implements Serializable {
 	}
 
 	public void toggleEditmode(ActionEvent even) {
-		editable=!editable;
+		editable = !editable;
 	}
 
 	public boolean isCheckbox() {
@@ -34,6 +47,58 @@ public class CheckboxBean implements Serializable {
 
 	public void setCheckbox(boolean checkbox) {
 		this.checkbox = checkbox;
+	}
+
+	public boolean isCbxError() {
+		return cbxError;
+	}
+
+	public void setCbxError(boolean errorCheckbox) {
+		this.cbxError = errorCheckbox;
+		if (errorCheckbox) {
+			FacesContext.getCurrentInstance().addMessage("messageForm:checkboxWithErrorMessage",
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "This is what an error looks like."));
+		}
+	}
+
+	public void action() {
+		System.out.println("Action");
+	}
+
+	public boolean isCbxFatal() {
+		return cbxFatal;
+	}
+
+	public void setCbxFatal(boolean cbxFatal) {
+		this.cbxFatal = cbxFatal;
+		if (cbxFatal) {
+			FacesContext.getCurrentInstance().addMessage("messageForm:checkboxWithFatalMessage",
+					new FacesMessage(FacesMessage.SEVERITY_FATAL, "", "This is what a fatal error looks like."));
+		}
+	}
+
+	public boolean isCbxWarning() {
+		return cbxWarning;
+	}
+
+	public void setCbxWarning(boolean cbxWarning) {
+		this.cbxWarning = cbxWarning;
+		if (cbxWarning) {
+			FacesContext.getCurrentInstance().addMessage("messageForm:checkboxWithWarning",
+					new FacesMessage(FacesMessage.SEVERITY_WARN, "", "This is what a warning looks like."));
+		}
+	}
+
+	public boolean isCbxInfo() {
+		return cbxInfo;
+	}
+
+	public void setCbxInfo(boolean cbxInfo) {
+		this.cbxInfo = cbxInfo;
+		if (cbxInfo) {
+			FacesContext.getCurrentInstance().addMessage("messageForm:checkboxWithInfoMessage",
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "", "This is what an info message looks like."));
+		}
 	}
 
 }
