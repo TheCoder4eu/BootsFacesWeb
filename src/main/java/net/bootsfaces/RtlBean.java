c65127b7c716655c7a4bc9f8316c8ed762810f51
stephan <webmaster1@beyondjava.de>
2016-01-03 19:29:37 +0100

    Documented the RTL support

---
+++ src/main/java/net/bootsfaces/RtlBean.java
@@ -0,0 +1,30 @@
+package net.bootsfaces;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean
+public class RtlBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private boolean rtl = true;
+
+	public boolean isRtl() {
+		return rtl;
+	}
+
+	public void setRtl(boolean rtl) {
+		this.rtl = rtl;
+	}
+
+	public String getDirection() {
+		return rtl ? "rtl" : "ltr";
+	}
+	
+	public void toggleRTL() {
+		rtl=!rtl;
+	}
+
+}
