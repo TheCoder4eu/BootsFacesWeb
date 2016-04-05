61b172b2cf3a1fbdd1565bf45d5f1f771c03b485
asterd <duda84@gmail.com>
2016-04-05 11:22:40 +0200

    Fix tree example page and add ScrollSpy examples

---
+++ src/main/java/test/beans/ScrollSpyBean.java
@@ -0,0 +1,21 @@
+package test.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import net.bootsfaces.component.scrollSpy.event.ScrollSpyEventListener;
+
+@SessionScoped
+@ManagedBean(name="spy")
+public class ScrollSpyBean implements ScrollSpyEventListener {
+
+	@Override
+	public void itemSelected(String arg0) {
+		System.out.println("SELECTED: " + arg0);
+		
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "SELECTED: " + arg0, "SELECTED: " + arg0));
+	}
+
+}
