7fc978746cfd9173c9804648e92f8f2765f81cae
stephan <webmaster1@beyondjava.de>
2016-05-28 23:54:22 +0200

    Started to document the b:gyroscope widget

--- src/main/java/net/bootsfaces/demo/GyroscopeBean.java
+++ src/main/java/net/bootsfaces/demo/GyroscopeBean.java
@@ -1,11 +1,52 @@
 package net.bootsfaces.demo;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean
+@ViewScoped
 public class GyroscopeBean {
+
+	private int alpha;
+	private int beta;
+	private int gamma;
+	private int calls = 0;
+
+	public int getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(int alpha) {
+		this.alpha = alpha;
+		calls++;
+	}
+
+	public int getBeta() {
+		return beta;
+	}
+
+	public void setBeta(int beta) {
+		this.beta = beta;
+	}
+
+	public int getGamma() {
+		return gamma;
+	}
+
+	public void setGamma(int gamma) {
+		this.gamma = gamma;
+	}
+
 	public void onRotation() {
 		System.out.println("Rotate! " + System.currentTimeMillis());
+	}
+
+	public int getCalls() {
+		return calls;
+	}
+
+	public void setCalls(int calls) {
+		this.calls = calls;
 	}
 
 }
