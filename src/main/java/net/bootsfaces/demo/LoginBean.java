043f9cb847cfaa6a18b8ec927741977a9df529dd
stephan <webmaster1@beyondjava.de>
2016-06-05 12:06:39 +0200

    #398 added an option to influence the name and the id of the input field (as opposed to the entire component, which also consists of the surrounding divs)

--- src/main/java/net/bootsfaces/demo/LoginBean.java
+++ src/main/java/net/bootsfaces/demo/LoginBean.java
@@ -1,29 +1,34 @@
 package net.bootsfaces.demo;
 
+import java.io.Serializable;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
 @ManagedBean
-@RequestScoped
-public class LoginBean {
+@ViewScoped
+public class LoginBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Size(min=4, max=10)
 	@NotEmpty
 	private String username;
-	
+
 	@Size(min=4, max=10)
 	@NotEmpty
 	private String password;
-	
+
 
 	public void setUsername(String name) {
 		this.username = name;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
@@ -37,18 +42,29 @@ public class LoginBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public void login() {
 		if ("BootsFaces".equalsIgnoreCase(username) && "rocks!".equalsIgnoreCase(password)) {
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Congratulations! You've successfully logged in.", "");
 			FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
-			
+
 		} else {
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "That's the wrong password. Hint: BootsFaces rocks!", "");
 			FacesContext.getCurrentInstance().addMessage("loginForm:password", msg);
 		}
 	}
-	
+
+	public void login2() {
+		if ("BootsFaces".equalsIgnoreCase(username) && "rocks!".equalsIgnoreCase(password)) {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Congratulations! You've successfully logged in.", "");
+			FacesContext.getCurrentInstance().addMessage("loginForm:passwordDiv", msg);
+
+		} else {
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "That's the wrong password. Hint: BootsFaces rocks!", "");
+			FacesContext.getCurrentInstance().addMessage("loginForm:passwordDiv", msg);
+		}
+	}
+
 	public void forgotPassword() {
 		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Default user name: BootsFaces", "");
 		FacesContext.getCurrentInstance().addMessage("loginForm:username", msg);
