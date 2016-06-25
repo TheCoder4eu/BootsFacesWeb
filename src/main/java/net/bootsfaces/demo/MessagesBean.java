35db16993ec3f8e2b4fbf11edb2d3379fceec79c
stephan <webmaster1@beyondjava.de>
2016-06-25 13:40:27 +0200

    #437 implemented and documented the 'for' attribute of b:messages

--- src/main/java/net/bootsfaces/demo/MessagesBean.java
+++ src/main/java/net/bootsfaces/demo/MessagesBean.java
@@ -5,12 +5,17 @@ package net.bootsfaces.demo;
  */
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 
 /**
  * @author Stephan Rauh, http://www.beyondjava.net
@@ -19,38 +24,86 @@ import javax.faces.event.ActionEvent;
 @RequestScoped
 public class MessagesBean implements Serializable {
 	private static final long serialVersionUID = 5497787734011747081L;
-	
+
+	@Size(min=1,max=8)
+	private String lastName;
+
+	@Size(min=3,max=7)
+	private String firstName;
+
+	@Min(3)
+	@Max(99)
+	private int age;
+
+	@Past
+	private Date birthday;
+
 	public void specificInfo() {
         FacesContext.getCurrentInstance().addMessage("growlForm:ref", new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "This is a specific message!"));
     }
-	
+
     public void info() {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "PrimeFaces <b>rocks</b>. BootsFaces <b>rocks</b>, too!"));
     }
-     
+
     public void warn() {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", "Watch out for <i>PrimeFaces</i>. It's a good match to BootsFaces."));
     }
-     
+
     public void error() {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Something has gone <strong>wrong</strong>."));
     }
-     
+
     public void fatal() {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error. What did <u>you</u> do?"));
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "Now it's too late to do anything. The system is down."));
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Please shut down all systems!"));
     }
-	
+
 	public void showMessages() {
 		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Info message", "This is a very informative message.");
 		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
-	
+
     public void info(ActionEvent event) {
     	String[] messages={"<b>BootsFaces</b> rocks!", "How do you like <b>this message</b>?", "This message has been brought to <b>you</b> by an actionListener."};
     	int index = (int)(Math.random()*messages.length);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", messages[index]));
     }
 
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public void update() {
+
+	}
 }
