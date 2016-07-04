c2c433a05626d825c83d0662bbf0af3d280b1c6f
stephan <webmaster1@beyondjava.de>
2016-07-04 22:26:36 +0200

    Added documentation

---
+++ src/main/java/net/bootsfaces/demo/ShakeBean.java
@@ -0,0 +1,26 @@
+/*
+ * A Bean Used for Demo purposes
+ */
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import net.bootsfaces.utils.FacesMessages;
+
+/**
+ *
+ * @author TheCoder4eu
+ */
+@ManagedBean
+@RequestScoped
+public class ShakeBean implements Serializable {
+	private static final long serialVersionUID = -1;
+
+	public void onShaken() {
+		FacesMessages.info("Shaken at " + new Date());
+	}
+}
