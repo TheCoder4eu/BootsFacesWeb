58373bdecc7ac0489e5b93451c63d9aa7116af8e
stephan <webmaster1@beyondjava.de>
2016-07-30 17:30:14 +0200

    Added a map example

--- src/main/java/net/bootsfaces/demo/RadiobuttonBean.java
+++ src/main/java/net/bootsfaces/demo/RadiobuttonBean.java
@@ -18,6 +18,9 @@
 package net.bootsfaces.demo;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -31,6 +34,17 @@ public class RadiobuttonBean implements Serializable {
 	private int javaVersion;
 
 	private String favoriteColor;
+
+	private String favoriteBand;
+
+	private Map<String, String> bands = new HashMap<String, String>();
+
+	public RadiobuttonBean() {
+		getBands().put("Arch Enemy", "melodic death metal");
+		getBands().put("Blind Guardian", "speed metal");
+		getBands().put("Fields of the Nephilim", "gothic metal");
+		getBands().put("Led Zeppelin", "rock");
+	}
 
 	public int getJavaVersion() {
 		return javaVersion;
@@ -48,11 +62,21 @@ public class RadiobuttonBean implements Serializable {
 		if (favoriteColor == null || favoriteColor.equals("")) {
 			FacesMessages.warning("@property(radiobuttonBean.favoriteColor)", "", "You seem to be a bit indecisive.");
 		} else if (favoriteColor.equals("Red")) {
-			FacesMessages.error("@property(radiobuttonBean.favoriteColor)", "", "Let me guess: you didn't think of traffic lights?");
+			FacesMessages.error("@property(radiobuttonBean.favoriteColor)", "",
+					"Let me guess: you didn't think of traffic lights?");
 		} else if (favoriteColor.equals("Green")) {
 			FacesMessages.info("@property(radiobuttonBean.favoriteColor)", "", "Someone ordered green?");
 		} else if (favoriteColor.equals("Blue")) {
-			FacesMessages.info("@property(radiobuttonBean.favoriteColor)", "", "Blue oceans are always nice to look at. Not to mention blue eyes. Or is it just that you've got the blues?");
+			FacesMessages.info("@property(radiobuttonBean.favoriteColor)", "",
+					"Blue oceans are always nice to look at. Not to mention blue eyes. Or is it just that you've got the blues?");
+		}
+	}
+
+	public void submitBand() {
+		if (favoriteBand == null || favoriteBand.equals("")) {
+			FacesMessages.warning("@property(radiobuttonBean.favoriteBand)", "", "You seem to be a bit indecisive.");
+		} else {
+			FacesMessages.info("@property(radiobuttonBean.favoriteBand)", "", "So you like " + favoriteBand + ".");
 		}
 	}
 
@@ -62,6 +86,22 @@ public class RadiobuttonBean implements Serializable {
 
 	public void setFavoriteColor(String favoriteColor) {
 		this.favoriteColor = favoriteColor;
+	}
+
+	public Map<String, String> getBands() {
+		return bands;
+	}
+
+	public void setBands(Map<String, String> bands) {
+		this.bands = bands;
+	}
+
+	public String getFavoriteBand() {
+		return favoriteBand;
+	}
+
+	public void setFavoriteBand(String favoriteBand) {
+		this.favoriteBand = favoriteBand;
 	}
 
 }
