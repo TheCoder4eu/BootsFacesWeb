36d8d2b36ad51544b34e4329584e259dc08d6047
stephan <webmaster1@beyondjava.de>
2016-07-31 14:37:56 +0200

    added a demo for HighFaces

---
+++ src/main/java/net/bootsfaces/demo/highFaces/HighFacesSettingsBean.java
@@ -0,0 +1,55 @@
+package net.bootsfaces.demo.highFaces;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class HighFacesSettingsBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String boysType="line";
+	
+	private String girlsType="column";
+	
+	private List<String> chartTypes = new ArrayList<String>();
+	
+	public HighFacesSettingsBean() {
+		chartTypes.add("line");
+		chartTypes.add("bar");
+		chartTypes.add("pie");
+		chartTypes.add("column");
+	}
+
+	public String getBoysType() {
+		return boysType;
+	}
+
+	public void setBoysType(String boysType) {
+		this.boysType = boysType;
+	}
+
+	public String getGirlsType() {
+		return girlsType;
+	}
+
+	public void setGirlsType(String girlsType) {
+		this.girlsType = girlsType;
+	}
+
+	public List<String> getChartTypes() {
+		return chartTypes;
+	}
+
+	public void setChartTypes(List<String> chartTypes) {
+		this.chartTypes = chartTypes;
+	}
+	
+	public void redraw() {
+	}
+}
---
+++ src/main/java/net/bootsfaces/demo/highFaces/LineChartBean.java
@@ -0,0 +1,38 @@
+package net.bootsfaces.demo.highFaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class LineChartBean {
+ 
+    protected List<Birth> boys;
+    protected List<Birth> girls;
+     
+    public List<Birth> getBoys() {
+        return boys;
+    }           
+     
+    public List<Birth> getGirls() {
+        return girls;
+    }   
+     
+    public LineChartBean() {
+        reload();
+    }
+ 
+    private void reload() {
+        boys = new ArrayList<>();
+        girls = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 2000; i < 2010; i++) {
+            boys.add(new Birth(Integer.toString(i), r.nextInt(500) + 800));   
+            girls.add(new Birth(Integer.toString(i), r.nextInt(500) + 800));   
+        }  
+    }
+}
