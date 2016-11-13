21228ee915e6635b7117b5928b91a79126fc434d
stephan <webmaster1@beyondjava.de>
2016-11-13 12:06:37 +0100

    tests for #475

---
+++ src/main/java/net/bootsfaces/demo/forms/FormBean.java
@@ -0,0 +1,62 @@
+package net.bootsfaces.demo.forms;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class FormBean {
+	private int inputText = 0;
+	private String color = "#ff0000";
+	private Date date = new Date();
+	private String combobox = "1";
+
+	public int getInputText() {
+		return inputText;
+	}
+
+	public void setInputText(int inputText) {
+		this.inputText = inputText;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getCombobox() {
+		return combobox;
+	}
+
+	public void setCombobox(String combobox) {
+		this.combobox = combobox;
+	}
+	
+	public void updateInputText() {
+		inputText = (int) (Math.random()*1000);
+	}
+	public void updateCombobox() {
+		combobox = String.valueOf((int) (Math.random()*3.0));
+	}
+	public void updateColor() {
+		color = "#"+Integer.toHexString((int) (Math.random()*0xffffff));
+	}
+	public void updateDate() {
+		date = new Date(date.getTime()+86400*1000);
+	}
+
+}
