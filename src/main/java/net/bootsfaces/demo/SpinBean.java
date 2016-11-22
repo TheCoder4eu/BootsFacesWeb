de99a1c91193a7a1a1b1006f22aeee047158d089
asterd <duda84@gmail.com>
2016-11-22 10:56:09 +0100

    Update examples for TouchSpin and DateTimePicker

--- src/main/java/net/bootsfaces/demo/SpinBean.java
+++ src/main/java/net/bootsfaces/demo/SpinBean.java
@@ -7,16 +7,20 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class SpinBean {
 	private double spinValue = 45f;
+	private int intSpinValue = 33;
 	
 	public SpinBean() { }
 	
 	public void button1() {
 		System.out.println("Spin value is: " + spinValue);
+		System.out.println("Int Spin value is: " + intSpinValue);
 	}
 	
 	public void button2() {
 		spinValue = 98f;
+		intSpinValue = 21;
 		System.out.println("Spin value is: " + spinValue);
+		System.out.println("Int Spin value is: " + intSpinValue);
 	}
 
 	public double getSpinValue() {
@@ -27,5 +31,15 @@ public class SpinBean {
 	public void setSpinValue(double spinValue) {
 		System.out.println("Called set value with: " + spinValue);
 		this.spinValue = spinValue;
+	}
+	
+	public int getIntSpinValue() {
+		System.out.println("Called INT set value of: " + intSpinValue);
+		return intSpinValue;
+	}
+	
+	public void setIntSpinValue(int value) {
+		System.out.println("Called INT set value with: " + value);
+		this.intSpinValue = value;
 	}
 }
