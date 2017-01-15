fc191f66c0a1b185e8f545fa3cdd4a797089d7bf
stephan <webmaster1@beyondjava.de>
2017-01-16 00:41:37 +0100

    added a demo for #585

--- src/main/java/net/bootsfaces/demo/BeanValidationBean.java
+++ src/main/java/net/bootsfaces/demo/BeanValidationBean.java
@@ -2,14 +2,20 @@ package net.bootsfaces.demo;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @ManagedBean
 @RequestScoped
-public class UserBean {
+public class BeanValidationBean {
+	@NotNull(message="Your name is as important as you are. Don't hesitate to fill this field!")
 	@Size(min=1, max=10)
 	private String name;
 	
+	@Min(value=18, message="In most countries, you're not considered a grown-up yet.")
+	@Max(value=65, message="In many countries, you've already reached retirement age.")
 	private int age;
 
 	public String getName() {
