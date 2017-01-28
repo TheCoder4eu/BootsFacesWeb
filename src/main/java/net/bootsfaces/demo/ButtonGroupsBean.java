fe0dfda08cf11c39b16fc8fe55bb2c9d2a90b1cd
stephan <webmaster1@beyondjava.de>
2017-01-28 22:23:13 +0100

    #587 added the new attribute "content-disabled"

---
+++ src/main/java/net/bootsfaces/demo/ButtonGroupsBean.java
@@ -0,0 +1,45 @@
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class ButtonGroupsBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private boolean contentDisabled = false;
+	private boolean invidualLinksDisabled = false;
+	private boolean dropButtonDisabled = false;
+
+	public boolean isContentDisabled() {
+		return contentDisabled;
+	}
+
+	public void setContentDisabled(boolean contentDisabled) {
+		this.contentDisabled = contentDisabled;
+	}
+
+	/**
+	 * Dummy method to handle the AJAX event
+	 */
+	public void onChange() {
+	}
+
+	public boolean isDropButtonDisabled() {
+		return dropButtonDisabled;
+	}
+
+	public void setDropButtonDisabled(boolean dropButtonDisabled) {
+		this.dropButtonDisabled = dropButtonDisabled;
+	}
+
+	public boolean isInvidualLinksDisabled() {
+		return invidualLinksDisabled;
+	}          
+
+	public void setInvidualLinksDisabled(boolean invidualLinksDisabled) {
+		this.invidualLinksDisabled = invidualLinksDisabled;
+	}
+}
