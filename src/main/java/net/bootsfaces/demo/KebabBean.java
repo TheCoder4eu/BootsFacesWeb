d0aac432fdd71c33973d061e21c902deda516747
stephan <webmaster1@beyondjava.de>
2017-03-12 23:34:31 +0100

    #587 added the new attribute "content-disabled"

---
+++ src/main/java/net/bootsfaces/demo/KebabBean.java
@@ -0,0 +1,27 @@
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class KebabBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private boolean contentDisabled = false;
+
+	public boolean isContentDisabled() {
+		return contentDisabled;
+	}
+
+	public void setContentDisabled(boolean contentDisabled) {
+		this.contentDisabled = contentDisabled;
+	}
+	
+	public void onChange() {
+		
+	}
+}
