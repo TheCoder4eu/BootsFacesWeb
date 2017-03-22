13b8dbd58097be43dc57e571eb8de444dbbaac7c
stephan <webmaster1@beyondjava.de>
2017-03-22 22:35:19 +0100

    #621 documentation of the legacy and standard style of displaying errors

---
+++ src/main/java/net/bootsfaces/demo/MessageStyleBean.java
@@ -0,0 +1,26 @@
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class MessageStyleBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private boolean useLegacyBootsFacesStyle = true;
+
+	public boolean isUseLegacyBootsFacesStyle() {
+		return useLegacyBootsFacesStyle;
+	}
+
+	public void setUseLegacyBootsFacesStyle(boolean useLegacyBootsFacesStyle) {
+		this.useLegacyBootsFacesStyle = useLegacyBootsFacesStyle;
+	}
+	
+	public void toggleSettings() {
+		this.useLegacyBootsFacesStyle=!this.useLegacyBootsFacesStyle;
+	}
+
+}
