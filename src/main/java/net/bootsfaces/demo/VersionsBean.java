3ee9081413d90c468e578e54141ad56e7461ca07
Barbaric <thecoder4eu@gmail.com>
2017-08-26 19:32:16 +0200

    Improve versions display in footer.

---
+++ src/main/java/net/bootsfaces/demo/VersionsBean.java
@@ -0,0 +1,43 @@
+package net.bootsfaces.demo;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import net.bootsfaces.listeners.AddResourcesListener;
+
+/**
+ * ManagedBean which holds the version numbers of used frameworks.
+ */
+@ManagedBean(name="versions")
+@ApplicationScoped
+public class VersionsBean {
+	private final static String bsfVersion, jsfImplementationVersion;
+//					primefacesVersion,
+//					omniFacesVersion,
+	
+
+	static {
+		bsfVersion = AddResourcesListener.class.getPackage().getImplementationVersion();
+//		primefacesVersion = RequestContext.getCurrentInstance().
+//								getApplicationContext().getConfig().getBuildVersion();
+//		omniFacesVersion =  Faces.class.getPackage().getImplementationVersion();
+		jsfImplementationVersion = FacesContext.class.getPackage().getImplementationVersion();
+	}
+
+//	public String getPrimefacesVersion() {
+//		return primefacesVersion;
+//	}
+
+	public String getBsfVersion() {
+		return bsfVersion;
+	}
+
+//	public String getOmnifacesversion() {
+//		return omniFacesVersion;
+//	}
+
+	public String getJsfImplementationVersion() {
+		return jsfImplementationVersion;
+	}
+}
