29080f4b831c55c1fe5e09cc62f30a04e1a33a97
stephan <webmaster1@beyondjava.de>
2017-09-03 21:39:06 +0200

    generate the navigator automatically

--- src/main/java/Collector.java
+++ src/main/java/Collector.java
@@ -1,32 +1,41 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Collector {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		scanFolder(new File("src/main/webapp"));
 	}
-	
-	public static void scanFolder(File folder) {
+
+	public static void scanFolder(File folder) throws IOException {
 		File[] files = folder.listFiles();
-		for (File file: files) {
+		for (File file : files) {
 			if (file.isDirectory()) {
 				scanFolder(file);
 			} else if (file.getName().endsWith(".xhtml")) {
 				if (!file.getName().endsWith("Attributes.xhtml")) {
 					String newFile = readFile(file.getAbsolutePath());
 					if (newFile != null) {
-						//System.out.println(file.getName());
+						writeFile(file.getAbsolutePath(), newFile);
+						System.exit(1);
 					}
 				}
- 			}
+			}
+		}
+	}
+
+	public static void writeFile(String filename, String content) throws IOException {
+		new File(filename).delete();
+		try (FileWriter fw = new FileWriter(filename); BufferedWriter bw = new BufferedWriter(fw)) {
+			bw.write(content);
 		}
 	}
 
@@ -36,7 +45,7 @@ public class Collector {
 		StringBuilder result = new StringBuilder();
 		String previousLine = "";
 
-		try(FileReader fr = new FileReader(filename); BufferedReader br= new BufferedReader(fr)) {
+		try (FileReader fr = new FileReader(filename); BufferedReader br = new BufferedReader(fr)) {
 			String line;
 
 			while ((line = br.readLine()) != null) {
@@ -55,7 +64,7 @@ public class Collector {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if (tags.size()>1) {
+		if (tags.size() > 1) {
 			int pos = result.indexOf("<ui:define name=\"content\">");
 			if (pos < 0) {
 				return null;
@@ -66,24 +75,27 @@ public class Collector {
 			String start = result.substring(0, pos);
 			String end = result.substring(pos);
 			StringBuilder navigator = new StringBuilder();
-			navigator.append("<ui:define name=\"content-navigator\">\n");
+			navigator.append("\n\n<ui:define name=\"content-navigator\">\n");
 			navigator.append("  <b:listLinks>\n");
-			for (String tag: tags) {
-				navigator.append("    <b:navLink href=\"#" + tag + "\" value=\""+ chapters.get(tag) + "\" />\n");
+			for (String tag : tags) {
+				navigator.append("    <b:navLink href=\"#" + tag + "\" value=\"" + chapters.get(tag) + "\" />\n");
 			}
-			
+
 			navigator.append("  </b:listLinks>\n");
 			navigator.append("</ui:define>\n\n");
 			System.out.println(filename + ": " + tags.size());
 			return start + navigator.toString() + end;
-			
+
 		}
 		return null;
 	}
 
 	private static String examineLine(String filename, Map<String, String> chapters, List<String> tags,
 			String previousLine, String line, String headerTag) {
-		String newHeaderTag = "<h1>";
+		String newHeaderTag = "<h2>";
+		if (headerTag.equals("<h1>")) {
+			newHeaderTag = "<h1>";
+		}
 		if (line.contains(headerTag)) {
 			if (line.contains("<a ")) {
 				int posh = line.indexOf(headerTag);
@@ -93,8 +105,7 @@ public class Collector {
 				}
 			} else if (previousLine.contains("<a ")) {
 				System.out.println("Previous line contains an anchor! " + line);
-			}
-			else {
+			} else {
 				// add the anchor tag
 				int start = line.indexOf(headerTag) + 4;
 				int end = line.indexOf("<", start);
@@ -105,13 +116,28 @@ public class Collector {
 					System.out.println("Weird!");
 				}
 				String innerText = line.substring(start, end).trim();
-				if (innerText.length()>0) {
-					String tag = innerText.replace(" ", "_").toLowerCase();
-					line = line.substring(0, start) + newHeaderTag + "<a id=" + tag + ">" + innerText + "</a>" + line.substring(end);
-					line.replace("</" + headerTag.substring(1), "</" + newHeaderTag.substring(1));
-					chapters.put(tag, innerText);
-					tags.add(tag);
-					newHeaderTag = "<h2>";
+				if (innerText.length() > 0) {
+					innerText = innerText.replace("(Virtually no)", "");
+					int end2 = 0;
+					while (end2 < innerText.length() && (Character.isLetter(innerText.charAt(end2))
+							|| Character.isWhitespace(innerText.charAt(end2)) || '&' == innerText.charAt(end2)
+							|| '/' == innerText.charAt(end2) || ';' == innerText.charAt(end2)
+							|| ':' == innerText.charAt(end2))) {
+						end2++;
+					}
+					String tag = innerText.substring(0, end2).replace("&lt;", "").replace("&gt;", "").replace(":", "_")
+							.replace("/", "").toLowerCase();
+					tag = tag.trim().replace(" ", "_");
+					if (tag.length() == 0) {
+						System.out.println("Empty tag?");
+					} else {
+						line = line.substring(0, line.indexOf(headerTag)) + newHeaderTag + "<a id=\"" + tag + "\">" + innerText
+								+ "</a>" + line.substring(end);
+						line = line.replace("</" + headerTag.substring(1), "</" + newHeaderTag.substring(1));
+						chapters.put(tag, innerText);
+						tags.add(tag);
+						newHeaderTag = "<h2>";
+					}
 				}
 			}
 		}
