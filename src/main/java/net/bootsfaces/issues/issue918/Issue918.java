87b9c17fe62725957ab3a25df559bde50d5c42ad
stephan <webmaster1@beyondjava.de>
2018-03-01 20:05:11 +0100

    TheCoder4eu/BootsFaces-OSP#918 trying to reproduce the bug

--- src/main/java/net/bootsfaces/issues/issue918/Issue918.java
+++ src/main/java/net/bootsfaces/issues/issue918/Issue918.java
@@ -3,59 +3,65 @@ package net.bootsfaces.issues.issue918;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @ViewScoped
-public class Issue918 implements Serializable {
+public class Issue918 implements Serializable
+{
+    Domains domains = new Domains();
 
-	private static final long serialVersionUID = 1L;
+    private List<Hazard_> hazards = new ArrayList<>();
 
-	private List<Hazard> hazards = new ArrayList<>();
+    HazardType_ lHazardType1 = new HazardType_();
+    HazardType_ lHazardType2 = new HazardType_();
+    HazardType_ lHazardType3 = new HazardType_();
 
-	private List<Hazard> potentialHazards = new ArrayList<>();
+    public Issue918()
+    {
+    	initialise();
+    }
 
-	private Hazard selectedHazard = null;
+    
+    private void initialise()
+    {
+        lHazardType1.setCode("type1");
+        lHazardType1.setText("type1");
+        lHazardType1.setHazardOrigins(domains.getHazardOrigins());
+        lHazardType2.setCode("type2");
+        lHazardType2.setText("type2");
+        lHazardType2.setHazardOrigins(domains.getHazardOrigins());
+        lHazardType3.setCode("type3");
+        lHazardType3.setText("type3");
+        lHazardType3.setHazardOrigins(domains.getHazardOrigins());
 
-	public Issue918() {
-		getHazards().add(new Hazard("1", "Fire"));
-		getHazards().add(new Hazard("2", "Inundation"));
-		getHazards().add(new Hazard("3", "Traffic"));
+        Hazard_ lHazard1 = new Hazard_();
+        lHazard1.setType(lHazardType1);
+        getHazards().add(lHazard1);
+        Hazard_ lHazard2 = new Hazard_();
+        lHazard2.setType(lHazardType2);
+        getHazards().add(lHazard2);
+        Hazard_ lHazard3 = new Hazard_();
+        lHazard3.setType(lHazardType3);
+        getHazards().add(lHazard3);
 
-		getPotentialHazards().add(new Hazard("4", "Storm"));
-		getPotentialHazards().add(new Hazard("5", "Drought"));
-		getPotentialHazards().add(new Hazard("6" ,"Famine"));
-	}
+    }
 
-	public List<Hazard> getHazards() {
-		return hazards;
-	}
+    public List<Hazard_> getHazards()
+    {
+        return hazards;
+    }
 
-	public void setHazards(List<Hazard> hazards) {
-		this.hazards = hazards;
-	}
+    public void setHazards(List<Hazard_> hazards)
+    {
+        this.hazards = hazards;
+    }
 
-	public void addNewHazard(Hazard selectedHazard) {
-		hazards.add(selectedHazard);
-	}
-
-	public List<Hazard> getPotentialHazards() {
-		return potentialHazards;
-	}
-
-	public void setPotentialHazards(List<Hazard> potentialHazards) {
-		this.potentialHazards = potentialHazards;
-	}
-
-	public Hazard getSelectedHazard() {
-		return selectedHazard;
-	}
-
-	public void setSelectedHazard(Hazard selectedHazard) {
-//		if (!"(Please select)".equals(selectedHazard)) {
-			this.selectedHazard = selectedHazard;
-//		}
-	}
+    public void addNewHazard()
+    {
+        Hazard_ lHazard = new Hazard_();
+        lHazard.setType(lHazardType1);
+        hazards.add(lHazard);
+    }
 }
