54ebddc01d49b287fa996147c36a3e239a36f42d
stephan <webmaster1@beyondjava.de>
2018-03-12 20:05:51 +0100

    added an example how to close a modal if and only if there are no validation violations

---
+++ src/main/java/net/bootsfaces/demo/ModalBean.java
@@ -0,0 +1,23 @@
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class ModalBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int backupPeriod = 42;
+
+	public int getBackupPeriod() {
+		return backupPeriod;
+	}
+
+	public void setBackupPeriod(int backupPeriod) {
+		this.backupPeriod = backupPeriod;
+	}
+	
+}
