77fd2ac15df8b2c2049d22c574f3d7eae42efe60
stephan <webmaster1@beyondjava.de>
2018-03-28 22:15:32 +0200

    added an example demonstrating how to put fields, labels, and messages into the same line

---
+++ src/main/java/net/bootsfaces/demo/forms/PersonBean.java
@@ -0,0 +1,73 @@
+package net.bootsfaces.demo.forms;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class PersonBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String firstName = "";
+	
+	private String lastName = "";
+	
+	private String zipCode ="";
+	private String city="";
+	
+	private String street="";
+	
+	private String poBox="";
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getPoBox() {
+		return poBox;
+	}
+
+	public void setPoBox(String poBox) {
+		this.poBox = poBox;
+	}
+
+}
