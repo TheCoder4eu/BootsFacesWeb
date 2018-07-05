6f995ac76cdc54fec4342d7f02cd02f5568ec64c
Matthew Broadhead <nbmlaw@gmail.com>
2018-07-05 17:11:24 +0200

    test command link with action

---
+++ src/main/java/net/bootsfaces/demo/forms/CommandLinkBean.java
@@ -0,0 +1,19 @@
+package net.bootsfaces.demo.forms;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import net.bootsfaces.utils.FacesMessages;
+
+@ManagedBean
+@ViewScoped
+public class CommandLinkBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public String testAction() {
+		FacesMessages.info("Command link clicked", "Detail?");
+		return null;
+	}
+}
