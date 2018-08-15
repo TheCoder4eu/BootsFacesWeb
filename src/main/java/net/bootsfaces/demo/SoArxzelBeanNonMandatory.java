4633fce03e8984005ae6c0698ade97e5da8d65e8
stephan <webmaster1@beyondjava.de>
2018-08-15 22:53:26 +0200

    trying to reproduce the bug report from StackOverflow https://stackoverflow.com/questions/51831950/fvalidateregex-for-not-required-inputtext-its-possible

---
+++ src/main/java/net/bootsfaces/demo/SoArxzelBeanNonMandatory.java
@@ -0,0 +1,51 @@
+/**
+ *  (C) 2013-2017 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+import net.bootsfaces.component.messages.Messages;
+import net.bootsfaces.utils.FacesMessages;
+
+/**
+ * This bean is used by the reproducer of  https://stackoverflow.com/questions/51831950/fvalidateregex-for-not-required-inputtext-its-possible.
+ * @author Stephan Rauh, http://www.beyondjava.net
+ */
+@ViewScoped
+@ManagedBean
+public class SoArxzelBeanNonMandatory implements Serializable {
+	private static final long serialVersionUID = -6810958868318611791L;
+	private String text="";
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void action() {
+		System.out.println("Action called!");
+		FacesMessages.info("Código enviado: " + text);
+	}
+}
