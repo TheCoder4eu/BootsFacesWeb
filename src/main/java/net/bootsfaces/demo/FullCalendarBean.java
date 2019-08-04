738e4b76e8e5733b7a66d07d7e4130b33230ac0b
stephan <webmaster1@beyondjava.de>
2019-08-04 13:41:54 +0200

    TheCoder4eu/BootsFaces-OSP#672 added examples for every supported language

---
+++ src/main/java/net/bootsfaces/demo/FullCalendarBean.java
@@ -0,0 +1,24 @@
+package net.bootsfaces.demo;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class FullCalendarBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String language = "en";
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	public void updateLanguage() {}
+}
