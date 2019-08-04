060e060f8942cbf083b6174c7941e9514b3db01e
stephan <webmaster1@beyondjava.de>
2019-08-04 16:04:33 +0200

    TheCoder4eu/BootsFaces-OSP#541 added the reproducer

---
+++ src/main/java/net/bootsfaces/issues/issue541/Issue541Bean.java
@@ -0,0 +1,40 @@
+package net.bootsfaces.issues.issue541;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named("issue541Bean")
+@ViewScoped
+public class Issue541Bean implements Serializable {
+
+    private String value = "Hallo";
+
+    private static final long serialVersionUID = 745232242454805589L;
+
+
+    boolean b = true;
+
+    public Issue541Bean() {
+        super();
+    }
+
+    public void onChange(){
+        System.out.println("##### > OK");
+        b = false;
+    }
+
+    public boolean isDisabled(){
+        return b;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    } 
+
+}
