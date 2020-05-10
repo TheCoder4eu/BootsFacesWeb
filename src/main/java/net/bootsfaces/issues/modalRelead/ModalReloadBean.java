c7ca6f8c2f8ec6de28113785a3026d5e2160ac5a
stephan <webmaster1@beyondjava.de>
2020-05-10 21:49:33 +0200

    now Gradle and Maven use the same version of the servet API

--- src/main/java/net/bootsfaces/issues/modalRelead/ModalReloadBean.java
+++ src/main/java/net/bootsfaces/issues/modalRelead/ModalReloadBean.java
@@ -1,8 +1,6 @@
 package net.bootsfaces.issues.modalRelead;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
